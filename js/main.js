30493313f957e463611f0b220f49a1c3fe438e91
leoauza <88103762+leoauza@users.noreply.github.com>
2026-01-02 12:35:36 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+fetch('data/cortos.json')
+  .then(response => response.json())
+  .then(cortos => {
+    const contenedor = document.getElementById('listado');
+
+    cortos.forEach(corto => {
+      const item = document.createElement('div');
+      item.innerHTML = `
+        <h2>${corto.titulo} (${corto.anio})</h2>
+        <p><strong>${corto.director}</strong> — ${corto.tecnica}</p>
+        <p>${corto.descripcion}</p>
+        <hr>
+      `;
+      contenedor.appendChild(item);
+    });
+  });
